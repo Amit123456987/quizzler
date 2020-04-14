bb24df969aba570baa1775230383f87ffa1aa0fb
Amit Maurya <amitmaurya11042000@gmail.com>
2020-04-14 12:33:00 +0530

    Application takes quiz and returns the score to the student.

---
+++ lib/Question.dart
@@ -0,0 +1,14 @@
+// Class Name always
+
+class Question{
+
+  String questionText;
+  bool questionAnswer;
+
+  // constructor of Question
+  Question({String q,bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quizbrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,8 +28,49 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  // List with five Icon Elements
+  // scoreKeeper.indexOf(element);
+  // scoreKeeper.add(element);
+  // scoreKeeper.insert(index,element);
+  List<Icon> scoreKeeper = [];
+
+  void CheckAnswer(bool UserPickedAnswer) {
+    setState(() {
+      if (quizBrain.getQuestionAnswer() == UserPickedAnswer)
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green, size: 22,));
+      else
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red, size: 22,));
+
+      if( quizBrain.getQuestionNumber() == quizBrain.getQuestionBankSize()-1 ){
+        Alert(
+          context: context,
+          title: "Quiz Ends",
+          desc: "Click On start Button to start it again...",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "START",
+                style: TextStyle(color: Colors.white, fontSize: 23),
+              ),
+              onPressed: () {
+                  Navigator.pop(context);
+                  scoreKeeper = [];
+                  quizBrain.startQuizAgain();
+              },
+              width: 120,
+            )
+          ],
+        ).show();
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Column/Row consists of multiple child or lists
+    // SafeArea/Padding consists of single child
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -37,7 +81,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                CheckAnswer(true);
               },
             ),
           ),
@@ -80,18 +124,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                CheckAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
